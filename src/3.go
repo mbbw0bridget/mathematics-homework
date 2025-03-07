04eeaf05dad2c783bc329c6c2e44395ad418788c
mbbw0bridget <mbbw0bridget@gmx.com>
2025-03-07 14:29:14 +0000

    Add 3.go

---
+++ src/3.go
@@ -0,0 +1,12 @@
+// Package main provides ...
+package main
+
+import "fmt"
+
+func main() {
+	// Calculate the sum of two numbers
+	num1 := 5
+	num2 := 10
+	sum := num1 + num2
+	fmt.Println("The sum is", sum)
+}
